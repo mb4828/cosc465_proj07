20dab487a592ac9bb0b06f274b6aff70a54ca428
Matt Brauner <mattbrauner44@gmail.com>
2014-04-12 23:22:26 -0400

    Coding complete. Time for testing

--- middlebox.py
+++ middlebox.py
@@ -19,6 +19,8 @@ import time
 class Middlebox(object):
     def __init__(self, net):
         self.net = net
+        self.clog = open('contentlog.txt','w')
+        self.acont = dict()                         # key "srcip:srcport:dstip:dstport"
 
     def main(self):    
         while True:
@@ -27,25 +29,99 @@ class Middlebox(object):
             except SrpyNoPackets:
                 continue
             except SrpyShutdown:
-                return
+                break
 
             # only deal with tcp packets; ignore everything else
             tcphdr = pkt.find('tcp')
             if tcphdr is not None:
-                log_debug("Got packet for TCP source: {}".format(pkt.dump()))
+                log_info("Got packet for TCP source: {}".format(pkt.dump()))
+                pktkey = self.getkey(pkt)
 
-                # your code should start at this indentation level; you're
-                # only concerned with TCP packets.  
+                # log packet data in application content dictionary
+                log_info("Packet data: {}".format(tcphdr.payload))
+                if pktkey not in self.acont.keys():
+                    self.acont[pktkey] = tcphdr.payload
+                else:
+                    self.acont[pktkey] += tcphdr.payload
 
+                # remove all instances of "NSA" and replace with "Fluffy Bunnies"
+                # also check for instances of panda
+                data = tcphdr.payload.split(" ")
+                kill = 0; changed = 0
+                for i in range(len(data)):
+                    if "NSA" in data[i]:
+                        log_info("Instance of 'NSA' detected")
+                        changed = 1
+                        j = data[i].find("NSA")
+                        data[i] = data[i][0:j] + "Fluffy Bunnies" + data[i][j+3:len(data[i])]
+                    if "panda" in data[i]:
+                        log_info("Red alert! Instance of 'panda' detected!")
+                        kill = 1
+                        tcphdr.RST = 1
+                        tcphdr.set_payload('')
+                        tcphdr.tcplen = 20
+                        break
 
+                # rebuild tcp payload and set tcplen
+                if not kill and changed:
+                    newdata = ""
+                    datalen = len(data)
+                    for i in range(datalen-1):
+                        newdata += data[i] + " "
+                    newdata += data[-1]
 
+                    log_info("Content is now harmless: {}".format(newdata))
+                    tcphdr.set_payload(newdata)
+                    tcphdr.tcplen = 20 + len(newdata)
+                else:
+                    log_info("No changes to data necessary")
 
-                # in the end, you should forward the packet out the same
-                # device (and the packet will almost certainly have been
-                # modified in some way before you send it back out.)
+                # rewrite destination ethernet address and rebuild packet
+                pkt.dst = self.getdst(pkt)
+                pkt.payload.payload = tcphdr
+
+                # is this the end of the TCP flow?
+                if tcphdr.FIN or tcphdr.RST:
+                    log_info("End of TCP flow. Writing to content log")
+                    self.writelog(pktkey)
+
+                # send packet back to switch
+                log_info("Finished packet: {}".format(pkt.dump()))
                 self.net.send_packet(dev, pkt)
 
+        # middlebox is shutting down
         log_info("Shutting down.")
+
+        for key in sorted(self.acont.keys()):
+            self.writelog(key)
+
+        self.clog.close()
+
+    def getdst(self, pkt):
+        '''
+        Computes the ethernet address of the destination
+        '''
+        n = str(pkt.payload.dstip)[-1]
+        dl = "00:00:00:00:00:0" + n
+        return EthAddr(dl)
+
+    def writelog(self, key):
+        '''
+        Appends collected data for application content entry "key" to the content log
+        with the format and removes the dictionary entry:
+            timestamp\tkey\tapplicationcontent\n
+        '''
+        line = time.asctime() + "\t" + key + "\t" + self.acont[key] + "\n"
+        self.clog.write(line)
+        del self.acont[key]
+
+    def getkey(self, pkt):
+        '''
+        Generates a key from a packet with the format "srcip:srcport:dstip:dstport"
+        '''
+        iphd = pkt.payload
+        tcphd = pkt.find('tcp')
+        return str(iphd.srcip) + ":" + str(tcphd.srcport) + ":" + str(iphd.dstip) + ":" + str(tcphd.dstport)
 
 
 def srpy_main(net):
--- p7.py
+++ p7.py
@@ -16,25 +16,56 @@ class p7(object):
         log.info("Got connection from {}".format(event.connection))
 
     def _handle_PacketIn (self, event):
-        inport = event.port # input port number on which packet arrived at switch
-        packet = event.parsed # reference to POX packet object
-        pktin = event.ofp # reference to Openflow PacketIn message (ofp_packet_in)
+        inport = event.port         # input port number on which packet arrived at switch
+        packet = event.parsed       # reference to POX packet object
+        pktin = event.ofp           # reference to Openflow PacketIn message (ofp_packet_in)
 
         if not packet.parsed:
             log.warning("{} {} ignoring unparsed packet".format(dpid, inport))
             return
 
         # packet is a "normal" POX packet object
+        log.debug("GOT A NEW PACKET")
         tcphdr = packet.find('tcp')
 
         if tcphdr is None:
             # for any non-TCP traffic, flood out all ports
-            pass
+            log.debug("packet is non-TCP traffic")
+            pktout = of.ofp_packet_out(data = pktin, 
+                                    actions = of.ofp_action_output(port=of.OFPP_FLOOD))
+            event.connection.send(pktout.pack())
 
         else: 
             # for any TCP traffic, install Openflow rules
-            pass
+            if ( (packet.srcip != IPAddr('10.0.0.4') and packet.dstip != IPAddr('10.0.0.4')) 
+                    or inport==5 ):
+                # packet is not suspicious or is from middlebox
+                # send directly to intended destination
+                log.debug("packet is TCP traffic, but it is safe")
+                outport = int(str(packet.dstip)[-1])
+                log.debug("outport = {}".format(outport))
 
+                actions = [ of.ofp_action_output(port = outport) ]
+                flowmod = of.ofp_flow_mod(command=of.OFPFC_ADD,
+                                    idle_timeout=10,
+                                    hard_timeout=10,
+                                    buffer_id=event.ofp.buffer_id,
+                                    match=of.ofp_match.from_packet(packet, inport),
+                                    actions=actions)
+            
+            else:
+                # packet is suspicious; forward to middlebox
+                log.debug("packet is suspicious; forward to middlebox")
+                actions = [ ofp_action_dl_addr.set_dst(EthAddr('00:00:00:00:00:05')),
+                            of.ofp_action_output(port=5) ]
+                flowmod = of.ofp_flow_mod(command=of.OFPFC_ADD,
+                                    idle_timeout=10,
+                                    hard_timeout=10,
+                                    buffer_id=event.ofp.buffer_id,
+                                    match=of.ofp_match.from_packet(packet, inport),
+                                    actions=actions)
+            
+            event.connection.send(flowmod.pack())
 
 def launch():
     core.registerNew(p7)
